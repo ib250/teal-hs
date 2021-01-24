498d3317598f9dfac7151427fc01f66f556cbfa6
Ismail Bello <ismail.bello.513@gmail.com>
2021-01-24 18:08:57 +0000

    chore: cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,16 @@
 module Main where
 
+--import qualified Data.Map as Map
+import qualified Data.Tree as Tree
 import qualified Teal
 
 
 parseAst :: String -> String
 parseAst teal = case Teal.parseFromStdIn teal of
-    Right expr -> "success...\n" ++ (show expr)
+    Right expr -> "success...\n" ++ (showTree expr)
     Left err -> "failure!!!\n" ++ (show err)
+    where showTree = Tree.drawForest . Teal.toForest
+
 
 
 main :: IO ()
--- src/Teal.hs
+++ src/Teal.hs
@@ -1,6 +1,8 @@
 
-module Teal where
+module Teal (parseFromStdIn, toForest) where
 
+import Data.List (intersperse)
+import qualified Data.Tree as Tree
 import qualified Teal.Expr as T
 import           Text.Parsec.Error
     ( ParseError
@@ -11,3 +13,33 @@ import qualified Text.ParserCombinators.Parsec as P
 
 parseFromStdIn :: String -> Either ParseError [T.TealExpr]
 parseFromStdIn = P.parse T.parseSource "stdin"
+
+
+toForest :: [T.TealExpr] -> Tree.Forest String
+toForest expr = Tree.unfoldForest alg expr
+    where alg :: T.TealExpr -> (String, [T.TealExpr])
+          alg (T.TealUnaryOp op expr) = (show op, [expr])
+          alg (T.TealBinaryOp op lhs rhs) = (show op, [lhs, rhs])
+          alg (T.TealIf c e1 e2) =
+            ( "IF"
+            , [c, T.TealList e1, T.TealList e2]
+            )
+
+          alg (T.TealApply f es) =
+            ( "Apply"
+            , [T.TealString f, T.TealList es]
+            )
+
+          alg (T.TealFunctionDecl (T.TealFunction name args es)) =
+            ( unwords [ "Declare"
+                      , name
+                      , "("
+                      , unwords $ intersperse "," args
+                      , ")"
+                      ]
+            , es
+            )
+          alg (T.TealImport s) = (unwords s, [])
+
+          -- simple case wrapped haskell values, containers etc...
+          alg xs = (show xs, [])
